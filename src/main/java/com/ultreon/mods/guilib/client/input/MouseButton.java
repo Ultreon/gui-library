30fef4462d460eedf0959ceb7305e47bb9a2cbae
quint <30625109+Qboi123@users.noreply.github.com>
2022-04-07 11:47:35 +0200

    Oops

---
+++ src/main/java/com/ultreon/mods/guilib/client/input/MouseButton.java
@@ -0,0 +1,12 @@
+package com.ultreon.mods.guilib.client.input;
+
+public class MouseButton {
+    public static final int LEFT = 0;
+    public static final int RIGHT = 1;
+    public static final int MIDDLE = 2;
+    public static final int FORWARD = 3;
+    public static final int BACKWARD = 4;
+    public static final int BUTTON_6 = 5;
+    public static final int BUTTON_7 = 6;
+    public static final int BUTTON_8 = 7;
+}
